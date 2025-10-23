df468da5d611cd0d72cd19a802819b1dac4f1764
ahmad fauzan isnata <160833065+ahmadfauzanisnata@users.noreply.github.com>
2025-10-23 08:38:14 +0700

    Memberikan jarak vertikal 25dp antara teks dan elemen berikutnya.

--- app/src/main/java/UIPertama.kt
+++ app/src/main/java/UIPertama.kt
@@ -16,4 +16,5 @@ fun AktivitasPertama(modifier: Modifier) {
             text = stringResource(id = R.string.univ),
             fontSize = 22.sp
         )
+        Spacer(modifier = Modifier.height(25.dp))
 
