6baf2e4a3f7b9155e09d46c59924c560f425a169
ahmad fauzan isnata <160833065+ahmadfauzanisnata@users.noreply.github.com>
2025-10-23 08:37:36 +0700

    Menampilkan teks lain dari resource R.string.univ dengan ukuran lebih kecil.

--- app/src/main/java/UIPertama.kt
+++ app/src/main/java/UIPertama.kt
@@ -12,4 +12,8 @@ fun AktivitasPertama(modifier: Modifier) {
             fontSize = 35.sp,
             fontWeight = FontWeight.Bold
         )
+        Text(
+            text = stringResource(id = R.string.univ),
+            fontSize = 22.sp
+        )
 
