b0773c3454a6651b13aac1a4d82ba94a38d8a72b
ahmad fauzan isnata <160833065+ahmadfauzanisnata@users.noreply.github.com>
2025-10-23 08:36:00 +0700

    Menyusun elemen UI secara vertikal (atas ke bawah).

--- app/src/main/java/UIPertama.kt
+++ app/src/main/java/UIPertama.kt
@@ -3,4 +3,8 @@
 
 @Composable
 fun AktivitasPertama(modifier: Modifier) {
+    Column(
+        modifier = Modifier.padding(top = 100.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
 
